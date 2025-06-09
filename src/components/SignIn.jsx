2b94c3bdb3c0ba1ca2e6be0538a050894a30e232
Juan Martin Lavalle <lavallejuanmartin@gmail.com>
2025-06-09 11:01:19 -0300

    Exercise 10.8 integrate Formik for form handling in SignIn component

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,64 @@
+import { Pressable, View, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
 import Text from './Text';
+import FormikTextInput from './FormikTextInput';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    display: 'flex',
+    backgroundColor: 'white',
+  },
+  signInButton: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 5,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  signInText: {
+    color: 'white',
+  },
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const onSubmit = (values) => {
+  console.log(values);
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => (
+        <View style={styles.inputContainer}>
+          <FormikTextInput name='username' placeholder='Username' />
+          <FormikTextInput
+            name='password'
+            placeholder='Password'
+            secureTextEntry
+          />
+          <Pressable onPress={handleSubmit} style={styles.signInButton}>
+            <Text
+              fontWeight='bold'
+              fontSize='subheading'
+              style={styles.signInText}
+            >
+              Sign In
+            </Text>
+          </Pressable>
+        </View>
+      )}
+    </Formik>
+  );
 };
 
 export default SignIn;
