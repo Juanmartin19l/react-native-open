eb404cbbd4b89c05d10154af04930d420533e062
Juan Martin Lavalle <lavallejuanmartin@gmail.com>
2025-06-12 11:54:30 -0300

    Exercise 10.9 and 10.10 Add Yup for form validation in SignIn component and update theme structure

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,6 +3,18 @@ import { Formik } from 'formik';
 import Text from './Text';
 import FormikTextInput from './FormikTextInput';
 import theme from '../theme';
+import * as yup from 'yup';
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required('Username is required')
+    .min(3, 'Username must be at least 3 characters long'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters long'),
+});
 
 const styles = StyleSheet.create({
   inputContainer: {
@@ -37,7 +49,11 @@ const onSubmit = (values) => {
 
 const SignIn = () => {
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
       {({ handleSubmit }) => (
         <View style={styles.inputContainer}>
           <FormikTextInput name='username' placeholder='Username' />
